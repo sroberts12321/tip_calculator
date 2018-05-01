a70799a512a27e7257167c0eeafcbe42c8e572dc
Stephen Roberts <Sroberts12321@gmail.com>
2018-05-01 11:36:31 -0500

    rounded tip to two decimals

--- tips.py
+++ tips.py
@@ -2,7 +2,7 @@ def tipper():
 	bill = float(input("Please input your bill $")) 
 	print ("$" + str(bill))
 	percent = float(input("What percentage of tip would you like to give? "))
-	tip = float((percent * .01) * bill)
+	tip = round(float((percent * .01) * bill), 2)
 	print("This is the tip amount: $" + str(float(tip)))
 	total = tip + bill
 	print("This is the total: $" + str(float(total)))
